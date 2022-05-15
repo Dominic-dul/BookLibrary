9a82d8b077287b21f14ca6da344456eabc8b9c13
Dominic-dul <dominykas.dulevicius@mif.stud.vu.lt>
2022-05-15 22:20:28 +0200

    add users

--- library.py
+++ library.py
@@ -15,17 +15,11 @@ def png():
     return send_from_directory('reports', 'showcase.png')
 
 
-# @app.route("/index")
-# def display_index():
-#     connection = psycopg2.connect(host="localhost", database="BooksDatabase", user="postgres", password="12344")
+@app.route("/")
+def index():
+    return render_template("index.html")
 
-#     cursor = connection.cursor()
-#     cursor.execute('SELECT * FROM books;')
-#     books = cursor.fetchall()
-#     cursor.close()
-#     connection.close()
-
-#     return render_template("index.html")
+#################################################################################################################################
 
 @app.route("/books")
 def show_books():
@@ -96,6 +90,301 @@ def modify_book(book_id):
         connection.close()
 
     return redirect("/books")
+
+#################################################################################################################
+
+@app.route("/users")
+def show_users():
+    connection = psycopg2.connect(host="localhost", database="BooksDatabase", user="postgres", password="12344")
+
+    cursor = connection.cursor()
+    cursor.execute('SELECT * FROM users;')
+    users_from_db = cursor.fetchall()
+    cursor.close()
+    connection.close()
+
+    nou = len(users_from_db)
+    return render_template("users.html", users = users_from_db, numberOfUsers = nou)
+
+@app.route("/user/delete/<user_id>")
+def delete_user(user_id):
+    connection = psycopg2.connect(host="localhost", database="BooksDatabase", user="postgres", password="12344")
+
+    connection.autocommit = True
+    cursor = connection.cursor()
+    cursor.execute("DELETE FROM users WHERE user_id = %s", (user_id,))
+
+    cursor.close()
+    connection.close()
+ 
+    return redirect("/users")
+
+@app.route("/user/add", methods=["GET", "POST"])
+def add_user():
+    if request.method == "GET":
+        return render_template("newUser.html")
+    if request.method == "POST":
+        id = str(uuid.uuid4())
+        name = request.form.get("name")
+        surname = request.form.get("surname")
+        nickname = request.form.get("nickname")
+
+    connection = psycopg2.connect(host="localhost", database="BooksDatabase", user="postgres", password="12344")
+    connection.autocommit = True
+    cursor = connection.cursor()
+    cursor.execute("INSERT INTO users (user_id, user_name, user_surname, user_nickname) VALUES (%s, %s, %s, %s)", (id, name, surname, nickname))
+    cursor.close()
+    connection.close()
+
+    return redirect("/users")
+
+@app.route("/user/modify/<user_id>", methods=["GET", "POST"])
+def modify_user(user_id):
+    if request.method == "GET":
+        connection = psycopg2.connect(host="localhost", database="BooksDatabase", user="postgres", password="12344")
+
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM users WHERE user_id = %s", (user_id,))
+        single_user = cursor.fetchone()
+        print(single_user)
+        cursor.close()
+        connection.close()
+
+        return render_template("modifyUser.html", user = single_user)
+
+    if request.method == "POST":
+        name = request.form.get("name")
+        surname = request.form.get("surname")
+        nickname = request.form.get("nickname")
+
+        connection = psycopg2.connect(host="localhost", database="BooksDatabase", user="postgres", password="12344")
+
+        connection.autocommit = True
+        cursor = connection.cursor()
+        cursor.execute("UPDATE users SET user_name = %s, user_surname = %s, user_nickname = %s WHERE user_id = %s", (name, surname, nickname, user_id))
+        cursor.close()
+        connection.close()
+
+        return redirect("/users")
+
+##################################################################################################################################
+
+@app.route("/lendings")
+def show_lendings():
+    connection = psycopg2.connect(host="localhost", database="library", 
+        user="postgres", password="12344")
+    cursor = connection.cursor()
+    cursor.execute("""SELECT books_users.id, books.book_name, users.user_name, until FROM books_users 
+        INNER JOIN books ON books_users.book_id = books.book_id 
+        INNER JOIN users ON books_users.user_id = users.user_id    
+        ORDER BY users.user_name, books.book_name
+    """)
+    lendings_from_db = cursor.fetchall()
+    cursor.close()
+    connection.close()
+
+    return render_template("lendings.html", entries = lendings_from_db,
+        number_of_lendings = len(lendings_from_db))
+
+@app.route("/lending/add", methods=["GET", "POST"])
+def add_lending():
+    if request.method == "GET":
+        connection = psycopg2.connect(host="localhost", database="library", user="postgres", password="12344")
+        
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM users;")
+        users_from_db = cursor.fetchall()
+        cursor.close()
+        
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM books;")
+        books_from_db = cursor.fetchall()
+        cursor.close()
+
+        connection.close()
+       
+        return render_template("lending_new.html", 
+            users = users_from_db, 
+            books = books_from_db)
+    if request.method == "POST":
+        id = str(uuid.uuid4())
+        user_id = request.form.get("user")
+        book_id = request.form.get("book")
+        until = request.form.get("until")
+
+        # take until as text - convert it to date and send to SQL
+
+        connection = psycopg2.connect(host="localhost", database="library", user="postgres", password="12344")
+        connection.autocommit = True
+        cursor = connection.cursor()
+        cursor.execute("""INSERT INTO books_users (id, user_id, book_id, until) 
+            VALUES (%s, %s, %s, %s)""", (id, user_id, book_id, until))
+        cursor.close()
+        connection.close()
+
+        return redirect("/lendings")
+
+@app.route("/lending/delete/<lending_id>")
+def delete_lending(lending_id):
+    connection = psycopg2.connect(host="localhost", 
+        database="library", user="postgres", password="12344")
+    connection.autocommit = True
+    cursor = connection.cursor()
+    cursor.execute("DELETE FROM books_users WHERE id = %s", (lending_id,))
+
+    cursor.close()
+    connection.close()
+
+    return redirect("/lendings")
+
+
+@app.route("/lending/modify/<lending_id>", methods=["GET", "POST"])
+def modify_lending(lending_id):
+    if request.method == "GET":
+        connection = psycopg2.connect(host="localhost", 
+            database="library", user="postgres", 
+            password="12344")
+        
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM users;")
+        users_from_db = cursor.fetchall()
+        cursor.close()
+        
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM books;")
+        books_from_db = cursor.fetchall()
+        cursor.close()
+        
+        cursor = connection.cursor()
+        cursor.execute("""SELECT id, book_id, user_id, until 
+            FROM books_users WHERE id = %s""", (lending_id,))
+        single_lending = cursor.fetchone()
+
+        # make sure lending contains valid date
+
+        cursor.close()
+        connection.close()
+
+        return render_template("lending_modify.html", 
+            lending = single_lending, 
+            users = users_from_db, 
+            books = books_from_db)
+
+    if request.method == "POST":
+        until = request.form.get("until")
+        book = request.form.get("book")
+        user = request.form.get("user")
+        
+        connection = psycopg2.connect(host="localhost", 
+            database="library", user="postgres", 
+            password="12344")
+        connection.autocommit = True
+        cursor = connection.cursor()
+        cursor.execute("""UPDATE books_users SET book_id = %s,
+            user_id = %s, until = %s WHERE id = %s""", 
+            (book, user, until, lending_id))
+        cursor.close()
+        connection.close()
+
+        return redirect("/lendings")  
+
+##########################################################################################################################################
+
+@app.route("/games")
+def show_games():
+    connection = psycopg2.connect(host="localhost", database="library", user="postgres", password="12344")
+
+    cursor = connection.cursor()
+    cursor.execute('SELECT * FROM games;')
+    games_from_db = cursor.fetchall()
+
+    cursor.close()
+    connection.close()
+
+    nog = len(games_from_db)
+    return render_template("games.html", games = games_from_db, numberOfGames = nog)
+
+@app.route("/game/delete/<game_id>")
+def delete_game(game_id):
+    connection = psycopg2.connect(host="localhost", 
+        database="library", user="postgres", password="12344")
+    connection.autocommit = True
+    cursor = connection.cursor()
+    cursor.execute("DELETE FROM games WHERE id = %s", (game_id,))
+
+    cursor.close()
+    connection.close()
+
+    return redirect("/games")
+
+@app.route("/game/add", methods=["GET", "POST"])
+def add_game():
+    if request.method == "GET":
+        return render_template("game_new.html")
+    if request.method == "POST":
+        game_id = str(uuid.uuid4())
+        name = request.form.get("name")
+
+    connection = psycopg2.connect(host="localhost", database="library", user="postgres", password="12344")
+    connection.autocommit = True
+
+    cursor = connection.cursor()
+    cursor.execute("INSERT INTO games (id, name) VALUES (%s, %s)", (game_id, name))
+    cursor.close()
+
+    id = str(uuid.uuid4())
+
+    cursor = connection.cursor()
+    cursor.execute("INSERT INTO game_parts (id, game_id) VALUES (%s, %s)", (id, game_id))
+    cursor.close()
+
+    connection.close()
+
+    return redirect("/games")
+
+
+@app.route("/game/modify/<game_id>", methods=["GET", "POST"])
+def modify_game(game_id):
+    if request.method == "GET":
+        connection = psycopg2.connect(host="localhost", 
+            database="library", user="postgres", 
+            password="12344")
+        
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM games WHERE id = %s;", (game_id,))
+        single_game = cursor.fetchall()
+        cursor.close()
+
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM game_parts WHERE game_id = %s;", (game_id,))
+        single_game_part = cursor.fetchall()
+        cursor.close()
+
+        connection.close()
+
+        return render_template("game_modify.html", game = single_game, game_part = single_game_part)
+
+    if request.method == "POST":
+        game_name = request.form.get("name")
+        game_part_name = request.form.get("game_part_name")
+        quantity = request.form.get("quantity")
+        
+        connection = psycopg2.connect(host="localhost", database="library", user="postgres", password="12344")
+        connection.autocommit = True
+
+        cursor = connection.cursor()
+        cursor.execute("UPDATE games SET name = %s WHERE id = %s", (game_name, game_id))
+        cursor.close()
+
+        cursor = connection.cursor()
+        cursor.execute("UPDATE game_parts SET name = %s, quantity = %s WHERE game_id = %s", (game_part_name, quantity, game_id))
+        cursor.close()
+
+        connection.close()
+
+        return redirect("/games")  
+
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, threaded=True, debug=True)
