9d53bdee9a4254230e3459f84eaed30cec236976
Dominic-dul <dominykas.dulevicius@mif.stud.vu.lt>
2022-05-16 09:39:09 +0200

    add css

--- library.py
+++ library.py
@@ -6,13 +6,13 @@ import psycopg2
 
 app = Flask(__name__)
 
-@app.route("/style.css")
-def css():
-    return send_from_directory('reports', 'style.css')
+# @app.route("/style.css")
+# def css():
+#     return send_from_directory('reports', 'style.css')
 
-@app.route("/showcase.png")
-def png():
-    return send_from_directory('reports', 'showcase.png')
+# @app.route("/showcase.png")
+# def png():
+#     return send_from_directory('reports', 'showcase.png')
 
 
 @app.route("/")
